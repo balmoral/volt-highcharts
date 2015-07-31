1b4e09d014bb1568a4428baa273140c318b98a8c
balmoral <colgunn@me.com>
2015-07-31 10:57:26 +1000

    use Volt::Model instead of hash for chart options

--- app/highcharts/controllers/main_controller.rb
+++ app/highcharts/controllers/main_controller.rb
@@ -8,17 +8,19 @@ module Highcharts
       container = self.container
       Volt.logger.debug("#{self.class.name}##{__method__}:#{__LINE__} : container='#{container}'")
 
-      opts = attrs.chart ? attrs.chart.to_h : missing_chart
+      opts = attrs.chart ? attrs.chart : missing_chart
+      opts = Volt::Model.new(opts.to_h) unless opts.is_a?(Volt::Model)
+      opts._id = random_id unless opts._id
 
       # If the view hasn't set a container id then
       # use the id in the chart options if present
       # otherwise to a random id.
-      id = `$(container).prop("id")`
+      dom_id = `$(container).prop("id")`
       Volt.logger.debug("#{self.class.name}##{__method__}:#{__LINE__} : initial container id is '#{id}'")
       unless id == nil || id == 'undefined'
-        id = opts[:id] || random_id
-        Volt.logger.debug("#{self.class.name}##{__method__}:#{__LINE__} : char container id is undefined - setting to '#{id}'")
-        `$(container).prop("id", id)`
+        dom_id = opts._id
+        Volt.logger.debug("#{self.class.name}##{__method__}:#{__LINE__} : char dom_id is undefined - setting to '#{dom_id}'")
+        `$(container).prop("id", dom_id)`
       end
 
       # If :renderTo has been set in the options then it
@@ -28,19 +30,24 @@ module Highcharts
       #
       # If :renderTo has not been set in the options then
       # it will be set here to the container id.
-      unless opts[:chart] && opts[:chart][:renderTo]
+      unless opts._chart && opts._chart._renderTo
         Volt.logger.debug("#{self.class.name}##{__method__}:#{__LINE__} : setting opts[:chart][:renderTo]='#{id}'")
-        (opts[:chart] ||= {})[:renderTo] = id
+        (opts._chart ||= {})._renderTo = id
       end
 
       # Render the chart and add it to the page._charts.
       # _charts ia an array of Volt::Models with an id and a chart attribute.
-      @id = opts[:id] || id
+      @id = opts._id
       chart = Native(`new Highcharts.Chart( #{opts.to_n} )`) # needs to wrapped for Volt::Model
       page._charts << Volt::Model.new({id: @id, chart: chart})
       page._chart = chart # a simple way for later access
-      page._chart_id = id # so we know whether it's me
+      page._chart_id = @id # so we know whether it's me
       Volt.logger.debug("#{self.class.name}##{__method__}:#{__LINE__} : page._charts='#{page._charts}' page._charts.size=#{page._charts.size}")
+
+      set_watches(opts)
+    end
+
+    def set_watches(opts)
     end
 
     def before_index_remove
