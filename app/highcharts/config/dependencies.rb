6a470b98aca04b64ef5c028254ab4642e5b0251b
balmoral <colgunn@me.com>
2015-08-03 15:28:15 +1000

    works in progress

--- app/highcharts/config/dependencies.rb
+++ app/highcharts/config/dependencies.rb
@@ -1,2 +1,4 @@
 # Component dependencies
 javascript_file 'http://code.highcharts.com/highcharts.src.js'
+javascript_file 'http://code.highcharts.com/stock/highstock.src.js'
+javascript_file 'http://code.highcharts.com/maps/highmaps.src.js'
