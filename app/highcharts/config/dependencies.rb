98f494503c412f8dd27bb9c8c25e245ed089dd25
balmoral <colgunn@me.com>
2015-08-11 09:24:39 +1000

    reactivity improvements, animate option

--- app/highcharts/config/dependencies.rb
+++ app/highcharts/config/dependencies.rb
@@ -2,7 +2,7 @@
 
 # highcharts.js is not required if highstock is loaded
 # javascript_file 'http://code.highcharts.com/highcharts.src.js'
-javascript_file 'http://code.highcharts.com/highcharts.src.js'
-javascript_file 'http://code.highcharts.com/highcharts-more.js'
-javascript_file 'http://code.highcharts.com/modules/exporting.js'
+javascript_file 'https://code.highcharts.com/highcharts.src.js'
+javascript_file 'https://code.highcharts.com/highcharts-more.js'
+javascript_file 'https://code.highcharts.com/modules/exporting.js'
 # javascript_file 'http://code.highcharts.com/maps/highmaps.src.js'
