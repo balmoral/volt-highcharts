418cdcdee6aad5e7bbea2a747c7654548d0567d8
balmoral <colgunn@me.com>
2015-08-23 17:09:54 +1000

    README.md edits

--- app/highcharts/config/dependencies.rb
+++ app/highcharts/config/dependencies.rb
@@ -1,5 +1,9 @@
 # Component dependencies
 
+if RUBY_PLATFORM == 'opal'
+  Opal.use_gem('opal-highcharts')
+end
+
 # highcharts.js is not required if highstock is loaded
 # javascript_file 'http://code.highcharts.com/highcharts.src.js'
 javascript_file 'https://code.highcharts.com/highcharts.src.js'
@@ -7,6 +11,3 @@ javascript_file 'https://code.highcharts.com/highcharts-more.js'
 javascript_file 'https://code.highcharts.com/modules/exporting.js'
 # javascript_file 'http://code.highcharts.com/maps/highmaps.src.js'
 
-if RUBY_PLATFORM == 'opal'
-  Opal.use_gem('opal-highcharts')
-end
