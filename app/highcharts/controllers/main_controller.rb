068ef52793032f6522f23a057a076a65ec53c8b3
balmoral <colgunn@me.com>
2015-08-23 11:15:23 +1000

    README.md edits

--- app/highcharts/controllers/main_controller.rb
+++ app/highcharts/controllers/main_controller.rb
@@ -92,7 +92,7 @@ module Highcharts
 
     def bind_series_data
       _series.each_with_index do |a_series, i|
-        bind_atomic(->{ a_series._data })
+        bind_atomic(->{ a_series._data }) do
           debug __method__, __LINE__, "chart.series[#{i}].set_data(#{a_series._data.to_a}, true, #{_animate})"
           chart.series[i].set_data(a_series._data.to_a, true, _animate)
         end
