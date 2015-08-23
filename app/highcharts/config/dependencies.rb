521772671876864641ff567d99f5ecc35a754dfb
balmoral <colgunn@me.com>
2015-08-23 17:09:36 +1000

    README.md edits

--- app/highcharts/config/dependencies.rb
+++ app/highcharts/config/dependencies.rb
@@ -7,4 +7,6 @@ javascript_file 'https://code.highcharts.com/highcharts-more.js'
 javascript_file 'https://code.highcharts.com/modules/exporting.js'
 # javascript_file 'http://code.highcharts.com/maps/highmaps.src.js'
 
-Opal.use_gem('opal-highcharts')
+if RUBY_PLATFORM == 'opal'
+  Opal.use_gem('opal-highcharts')
+end
